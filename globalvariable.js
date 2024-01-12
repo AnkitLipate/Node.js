38468f34772613e7071374ce82a237924fdd20b0
AnkitLipate <130392155+AnkitLipate@users.noreply.github.com>
2024-01-12 13:21:13 +0530

    Add files via upload

---
+++ globalvariable.js
@@ -0,0 +1,8 @@
+console.log("current folder name "+__dirname); //to get current directory name
+console.log("current file name "+__filename); //to get current file name
+console.log("proccess object");
+console.log(process.env);
+console.log("platform used"+process.platform);
+console.log(process.release);
+console.log("global object");
+console.log(global);
